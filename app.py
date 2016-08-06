a2a22722a3a036b0f15444fb4d0487ea18b9c4f2
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-08-06 14:46:20 +0900

    Configをクラスにする

--- app.py
+++ app.py
@@ -1,56 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import argparse
-import getpass
-import json
 import logging
-import os
-
-from pgoapi import pgoapi
-from pgoapi import utilities
 
 from modules.api import Api
+from modules.config import Config
 
 log = logging.getLogger(__name__)
-
-
-def init_config():
-    parser = argparse.ArgumentParser()
-    config_file = "config.json"
-
-    # If config file exists, load variables from json
-    load = {}
-    if os.path.isfile(config_file):
-        with open(config_file) as data:
-            load.update(json.load(data))
-
-    # Read passed in Arguments
-    required = lambda x: not x in load
-    parser.add_argument("-a", "--auth_service", help="Auth Service ('ptc' or 'google')",
-                        required=required("auth_service"))
-    parser.add_argument("-u", "--username", help="Username", required=required("username"))
-    parser.add_argument("-p", "--password", help="Password")
-    parser.add_argument("-l", "--location", help="Location", required=required("location"))
-    parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
-    parser.add_argument("-t", "--test", help="Only parse the specified location", action='store_true')
-    parser.set_defaults(DEBUG=False, TEST=False)
-    config = parser.parse_args()
-
-    # Passed in arguments shoud trump
-    for key in config.__dict__:
-        if key in load and config.__dict__[key] == None:
-            config.__dict__[key] = str(load[key])
-
-    if config.__dict__["password"] is None:
-        log.info("Secure Password Input (if there is no password prompt, use --password <pw>):")
-        config.__dict__["password"] = getpass.getpass()
-
-    if config.auth_service not in ['ptc', 'google']:
-        log.error("Invalid Auth service specified! ('ptc' or 'google')")
-        return None
-
-    return config
 
 
 def main():
@@ -63,9 +19,7 @@ def main():
     logging.getLogger("rpc_api").setLevel(logging.INFO)
 
     # コンフィグを取得
-    config = init_config()
-    if not config:
-        return
+    config = Config()
 
     # コンフィグ情報を元に各ライブラリのログレベルを調整
     if config.debug:
@@ -76,6 +30,6 @@ def main():
     api = Api(config.location)
     api.authenticate(config.auth_service, config.username, config.password)
 
-
+    
 if __name__ == '__main__':
     main()
---
+++ modules/config.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import getpass
+import json
+import logging
+import os
+
+from modules.exceptions import GeneralPokemonBotException
+
+log = logging.getLogger(__name__)
+
+
+class Config(object):
+    def __init__(self, config_file="config.json"):
+        parser = argparse.ArgumentParser()
+
+        # If config file exists, load variables from json
+        load = {}
+        if os.path.isfile(config_file):
+            with open(config_file) as data:
+                load.update(json.load(data))
+
+        # Read passed in Arguments
+        required = lambda x: not x in load
+        parser.add_argument("-a", "--auth_service", help="Auth Service ('ptc' or 'google')",
+                            required=required("auth_service"))
+        parser.add_argument("-u", "--username", help="Username", required=required("username"))
+        parser.add_argument("-p", "--password", help="Password")
+        parser.add_argument("-l", "--location", help="Location", required=required("location"))
+        parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
+        parser.add_argument("-t", "--test", help="Only parse the specified location", action='store_true')
+        parser.set_defaults(DEBUG=False, TEST=False)
+        config = parser.parse_args()
+
+        # Passed in arguments shoud trump
+        for key in config.__dict__:
+            if key in load and config.__dict__[key] == None:
+                config.__dict__[key] = str(load[key])
+
+        if config.__dict__["password"] is None:
+            log.info("Secure Password Input (if there is no password prompt, use --password <pw>):")
+            config.__dict__["password"] = getpass.getpass()
+
+        if config.auth_service not in ['ptc', 'google']:
+            raise GeneralPokemonBotException("Invalid Auth service specified! ('ptc' or 'google')")
+
+        self.config = config
+
+    def __getattr__(self, attr):
+        return self.config.__dict__[attr]
